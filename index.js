97688c4d15753119fa64322bb4b73463e6a1c926
Peter Hancock <peter.hancock@gmail.com>
2015-09-13 14:02:44 +0100

    wip

---
+++ index.js
@@ -0,0 +1,18 @@
+var fs = require('fs'),
+    mustache = require('mustache'),
+    concat = require('concat-stream'),
+    through = require('through2')
+
+module.exports = function(view) {
+
+    var t = through()
+    var v = Object.assign({}, view, {
+        bundle: view.srcfile ? view.srcfile.replace(/\.js$/, '-bundle.js') : 'bundle.js'
+    })
+
+    fs.createReadStream(__dirname + '/page.template').pipe(concat(function (template) {
+        t.end(mustache.render(template.toString(), v))
+    }))
+
+    return t
+}
