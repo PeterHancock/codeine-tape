3d2963b46b155fa2d787426f6e336ad381551029
Peter Hancock <peter.hancock@gmail.com>
2015-09-14 18:01:40 +0100

    Option to customize script name

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ var fs = require('fs'),
     concat = require('concat-stream'),
     through = require('through2')
 
-module.exports = function(view) {
+module.exports = function(view, opts) {
     var t = through()
+    var bundle = view.srcfile ? (opts.resolveTapeScript ? opts.resolveTapeScript(view.srcfile) : view.srcfile.replace(/\.js$/, '-bundle.js')) : null
     var v = Object.assign({}, view, {
-        bundle: view.srcfile ? view.srcfile.replace(/\.js$/, '-bundle.js') : 'bundle.js'
+        bundle: bundle
     })
 
     fs.createReadStream(__dirname + '/page.template').pipe(concat(function (template) {
         t.end(mustache.render(template.toString(), v))
     }))
-
     return t
 }
